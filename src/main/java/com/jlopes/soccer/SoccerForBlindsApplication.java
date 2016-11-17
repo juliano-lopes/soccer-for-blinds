3f7bc2634dac5af7d99c24172e4d8cc3aaa45707
juliano-lopes <julopys@hotmail.com>
2016-11-17 12:09:20 -0200

    Fixing class 'SoccerForBlindsApplication'.

--- src/main/java/com/jlopes/soccer/SoccerForBlindsApplication.java
+++ src/main/java/com/jlopes/soccer/SoccerForBlindsApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SoccerForBlindsApplication {
-
 	public static void main(String[] args) {
-
 		SpringApplication.run(SoccerForBlindsApplication.class, args);
-
 	}
 }
